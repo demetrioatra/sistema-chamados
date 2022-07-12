0c468fc3e4966fdf4fad3f1c7d1a176de994fea1
Demétrio Alves Atra <datra@verg1759.lobtec.com.br>
2022-07-12 10:58:16 -0300

    v. 1.0.2

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -1,22 +1,23 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey:             "AIzaSyDyignxcXRsbUhv5jD_Cq2s47z127orLJg",
-    authDomain:         "sistema-chamados-70587.firebaseapp.com",
-    projectId:          "sistema-chamados-70587",
-    storageBucket:      "sistema-chamados-70587.appspot.com",
-    messagingSenderId:  "95413725135",
-    appId:              "1:95413725135:web:bf2cdc83a001e597fad4ba",
-    measurementId:      "G-4LZ02YYF4N"
+  apiKey:               "AIzaSyDyignxcXRsbUhv5jD_Cq2s47z127orLJg",
+  authDomain:           "sistema-chamados-70587.firebaseapp.com",
+  projectId:            "sistema-chamados-70587",
+  storageBucket:        "sistema-chamados-70587.appspot.com",
+  messagingSenderId:    "95413725135",
+  appId:                "1:95413725135:web:bf2cdc83a001e597fad4ba",
+  measurementId:        "G-4LZ02YYF4N"
 };
-  
+
 // Initialize Firebase
-if (!firebase.apps.length) {
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
-}
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default app
